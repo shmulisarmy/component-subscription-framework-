f7d70b9668cf0d893ea14e56928eb3bf56c9030a
shmuli <shmulikeller@gmail.com>
2024-06-27 11:29:08 -0400

    added_filter

--- static/main.js
+++ static/main.js
@@ -72,6 +72,28 @@ function sortChildrenElements(main, selector, attr) {
 }
 
 
+function filter_children(parrentEelement, selector, attr, value){    
+    Array.from(parrentEelement.children).forEach(child => {
+        const withinChildElement = child.querySelector(selector);
+    if (withinChildElement && withinChildElement[attr] != value) {
+        child.remove();
+    }
+});
+
+
+}
+
+
+function filter_out_children(parrentEelement, selector, attr, value){    
+    Array.from(parrentEelement.children).forEach(child => {
+        const withinChildElement = child.querySelector(selector);
+    if (withinChildElement && withinChildElement[attr] == value) {
+        child.remove();
+    }
+});
+}
+
+
 
 
 function rerender_nearest_subscribed_element(element) {
@@ -156,57 +178,6 @@ function update_data_and_subscribers(data, key, new_data){
 
 const subscriptions = {};
 let subscription_keys_used = 0
-
-let menu = [
-    {
-        category: 'plate', 
-        options: [
-            {foodName: 'crispy chicken', price: 14, image_url: "static/images/1.jpg"},
-            {foodName: 'grilled salmon', price: 18, image_url: "static/images/2.jpg"},
-            {foodName: 'beef steak', price: 22, image_url: "static/images/3.jpg"}
-        ]
-    },
-    {
-        category: 'appetizer', 
-        options: [
-            {foodName: 'chicken nuggets', price: 10, image_url: "static/images/4.jpg"} ,
-            {foodName: 'mozzarella sticks', price: 8, image_url: "static/images/5.jpg"},
-            {foodName: 'stuffed mushrooms', price: 12, image_url: "static/images/6.jpg"}
-        ]
-    },
-    {
-        category: 'sandwich',
-        options: [
-            {foodName: 'turkey club', price: 12, image_url: "static/images/7.jpg"},
-            {foodName: 'ham and cheese', price: 10, image_url: "static/images/8.jpg"},
-            {foodName: 'veggie delight', price: 9, image_url: "static/images/9.jpg"}
-        ]
-    },
-
-    {
-        category: 'dessert',
-        options: [
-            {foodName: 'chocolate cake', price: 7, },
-            {foodName: 'cheesecake', price: 8},
-            {foodName: 'ice cream sundae', price: 6}
-        ]
-    },
-    {
-        category: 'beverage',
-        options: [
-            {foodName: 'coffee', price: 3, image_url: "image.jpg"},
-            {foodName: 'iced tea', price: 4},
-            {foodName: 'lemonade', price: 4}
-        ]
-    },
-];
-
-
-
-
-
-
-
 
 
 
